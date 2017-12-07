7854c700590ecc5fb389a292d16183bff2181b16
marekbaranowski <mbrnwsk@gmail.com>
2017-12-07 01:15:23 +0100

    Added UI tests

--- src/components/Table/__tests__/TableUI.spec.js
+++ src/components/Table/__tests__/TableUI.spec.js
@@ -1,12 +1,31 @@
 import React from "react";
 import toJson from "enzyme-to-json";
-import { Table } from "../index";
-import { FILTER_TYPES, COLUMNS } from "config";
+import { COLUMNS, ASYNC_STATES } from "config";
 import records from "store/__mockData__/records";
 import DataStore from "store/DataStore";
-import { data, fetchData } from "store/data";
+import { Table } from "../index";
 
-test.skip("renders correctly", () => {
+test("loading message displayed when data fetching", () => {
+  const tableProps = {
+    store: DataStore.create({ columns: COLUMNS, requestStatus: { status: ASYNC_STATES.PENDING } })
+  };
+
+  const wrapper = mount(<Table {...tableProps} />);
+
+  expect(toJson(wrapper)).toMatchSnapshot();
+});
+
+test("error message displayed when promise rejected", () => {
+  const tableProps = {
+    store: DataStore.create({ columns: COLUMNS, requestStatus: { status: ASYNC_STATES.REJECTED } })
+  };
+
+  const wrapper = mount(<Table {...tableProps} />);
+
+  expect(toJson(wrapper)).toMatchSnapshot();
+});
+
+test("full UI", () => {
   const tableProps = {
     store: DataStore.create({ columns: COLUMNS, records })
   };
