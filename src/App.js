8ac298a97b455557357f50b6d2385c1c20c52269
marekbaranowski <mbrnwsk@gmail.com>
2017-12-14 04:17:41 +0100

    Added info about errors

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import Table from "./components/Table";
 const App = () => (
   <div id="app" className="w-75 mx-auto">
     <h1 className="mt-5">Interactive table</h1>
-    <p className="mt-3 mb-5">Feel free to play with filters and sorting.</p>
+    <p className="mt-3 mb-5">
+      Feel free to play with filters and sorting. <span className="text-muted">
+        PLEASE NOTE: occasional errors are intended.
+      </span>
+    </p>
     <Table />
   </div>
 );
